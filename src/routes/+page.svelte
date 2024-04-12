c76e383211a038e6fac69f81946861a68ed8da18
Maxime <maxime.condaminat@gmail.com>
2024-04-12 15:28:57 +0200

    feat: add source code button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,9 @@
 </script>
 
 <main class="grid min-h-screen place-items-center">
+	<p class="fixed bottom-4 left-4 z-10 rounded-md bg-gray-900 px-4 py-2 text-sm text-gray-100">
+		<a class="underline" href="https://github.com/maximecd/carousel-menu">Source code</a>
+	</p>
 	<p class="fixed bottom-4 right-4 z-10 rounded-md bg-blue-100 px-4 py-2 text-sm text-blue-900">
 		Initial design by <a class="underline" href="https://www.decathlon.co.uk/"
 			>https://www.decathlon.co.uk/</a
